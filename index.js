e76d66c19d7cde69e740e4e0ed12a8e916bb19a2
Alex <belohvostov.belohvostov@gmail.com>
2022-08-15 09:56:16 +0300

    added cors

--- index.js
+++ index.js
@@ -1,38 +1,35 @@
-import 'dotenv/config'
+import "dotenv/config";
 import express from "express";
 import mongoose from "mongoose";
+import cors from "cors";
 
-import {
-  router
-} from './router/index.js';
+import { router } from "./router/index.js";
 
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 5000;
 
 const app = express();
 
 app.use(express.json());
-app.use("/uploads", express.static("uploads"))
-app.use("/api", router)
-
-
+app.use(cors());
+app.use("/uploads", express.static("uploads"));
+app.use("/api", router);
 
 const start = async () => {
   try {
-    await mongoose
-      .connect(
-        "mongodb+srv://admin:wwwwww@cluster0.epstaca.mongodb.net/blog?retryWrites=true&w=majority"
-      )
+    await mongoose.connect(
+      "mongodb+srv://admin:wwwwww@cluster0.epstaca.mongodb.net/blog?retryWrites=true&w=majority"
+    );
 
     app.listen(PORT, (err) => {
       if (err) {
         return console.log(err);
       }
 
-      console.log("Server is ready");
+      console.log(`Server is ready on ${PORT} port`);
     });
   } catch (error) {
-    console.log(error.message)
+    console.log(error.message);
   }
-}
+};
 
-start()
+start();
